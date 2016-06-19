47b18de78a415826c57ea83ac7126d7fda467eda
Juan Miguel Navarro Martínez <juanmi.3000@gmail.com>
2016-06-19 22:05:29 +0200

    Fix forms and tables inside the form

--- login.php
+++ login.php
@@ -11,11 +11,23 @@
 <div class="tab-content">
 	<div id="userpass" class="tab-pane fade in active">
 		<form action = "index.php?action=login" method = "post" name = "login_form">
-			<div id="inputlogin">Usuario: &nbsp;&nbsp;&nbsp;<input type = "text" name = "usuario" /></div>
-			<div id="inputlogin">Password: <input type = "password" name = "password" id = "password"/></div>
-
-
-			<input class="margXL" type = "submit" value = "Login" />
+			<table>
+				<tr>
+					<td>Usuario: </td>
+					<td>
+						<input type = "text" name = "usuario" />
+					</td>
+				</tr>
+				<tr>
+					<td>Password: </td>
+					<td><input type = "password" name = "password" id = "password"/></td>
+				</tr>
+				<tr>
+					<td colspan="2" style="direction: rtl">
+						<input class="margXL" type = "submit" value = "Login" />
+					</td>
+				</tr>
+			</table>
 		</form>
 		<a href="forgotpassword.php">¿Olvidaste su contraseña?</a>
 	</div>
@@ -26,25 +38,24 @@
 		<form action='./index.php?action=register' method='post'>
 			<table border='0'>
 				<tr>
-				<td>Usuario</td>
-				<td><input type='text' name='usuario' /></td>
+					<td>Usuario</td>
+					<td><input type='text' name='usuario' /></td>
 				</tr>
 				<tr>
-				<td>Correo electronico</td>
-				<td><input type='text' name='email' /></td>
+					<td>Correo electronico</td>
+					<td><input type='text' name='email' /></td>
 				</tr>
 				<tr>
-				<td>Contraseña</td>
-				<td><input type='password' name='password' /></td>
+					<td>Contraseña</td>
+					<td><input type='password' name='password' /></td>
 				</tr>
-				<td>Repite contraseña</td>
-				<td><input type='password' name='repassword' /></td>
+					<td>Repite contraseña</td>
+					<td><input type='password' name='repassword' /></td>
 				</tr>
 				<tr>
-				<td></td>
-				<td>
-				<input type="submit" name="save" value="Crear" />
-				</td>
+					<td colspan="2" style="direction: rtl">
+						<input type="submit" name="save" value="Crear" />
+					</td>
 				</tr>
 			</table>
 		</form>
